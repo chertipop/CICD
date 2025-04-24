f4f4fef906b6894ebbccc844a745029d555b122d
chertipop <kittiphopsrichiangsa@gmail.com>
2025-04-24 19:33:17 +0700

    update app.js and app.test.js

--- src/app.js
+++ src/app.js
@@ -3,11 +3,21 @@ const dayOfTheWeek = (date = new Date()) => {
 
     return days[date.getDay()];
 }
+const monthsOfYear = (date = new Date()) =>{ 
+    const month= [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+];
+return month[date.getMonth()];
+}
+
 
 
 try {
     document.getElementById('day').innerText = dayOfTheWeek();
+    document.getElementById('month').innerText = monthsOfYear();
 } catch(err) {}
 
 
 exports.dayOfTheWeek = dayOfTheWeek;
+exports.monthsOfYear=monthsOfYear;
